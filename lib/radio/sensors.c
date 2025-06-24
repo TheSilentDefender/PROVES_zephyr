399ceb9c044beb85cad444c94174ef8b80888175
Madhav Kapa <madhavkapa16@gmail.com>
2025-06-23 21:00:58 -0400

    github workflow test

---
+++ lib/radio/sensors.c
@@ -0,0 +1,10 @@
+#include <app/lib/radio.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/zbus/zbus.h>
+
+#include "zephyr/logging/log.h"
+LOG_MODULE_REGISTER(radio, LOG_LEVEL_INF);
+
+}
